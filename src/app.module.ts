db01e39e7df40e804187f6195c34ef9865ebeedc
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2023-09-21 23:45:22 +0200

    feat: first controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DogsModule } from './dogs/dogs.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
