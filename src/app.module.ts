9d7c138ac7a16a6b95dc00179a4442411adb4d2d
Ramazan Özdemir <ramazanozdemir2607@gmail.com>
2023-09-22 01:02:26 +0200

    feat: cats controled is complated

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DogsModule } from './dogs/dogs.module';
+import { CatsController } from './cats/cats.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), DogsModule],
-  controllers: [AppController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Query,
+  Post,
+  Redirect,
+  Req,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
   @Get()
   findAll(): string {
     return 'This action returns all cats';
+  }
+
+  // request payload (Body)
+  @Post()
+  create(@Body() requestBody) {
+    return { requestBody };
+  }
+
+  // changed status code
+  @Get('status')
+  @HttpCode(204)
+  path(@Req() req: Request): string {
+    console.log({ req });
+    return 'This action request test';
+  }
+
+  //redirected
+  @Get('redirect')
+  @Redirect('https://google.com', 301)
+  redirect(@Query('version') version) {
+    console.log('warum', version);
+    // if the action returns any thing , the redirect decorator is overridden
+    if (version == 5) {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
+  }
+
+  // basic express response object
+  @Get('express')
+  express(@Res() response: Response) {
+    return response
+      .status(HttpStatus.OK)
+      .send('This action returns by express response');
+  }
+
+  // handling route params
+  @Get(':id')
+  findOne(@Param() params: any): string {
+    console.log(params.id);
+    return `This action returns a #${params.id} cat`;
   }
 }
